02df24dca6ebf05ae829a6a7639522801b9bbef0
Thales Mello <thalesmello@gmail.com>
2016-07-21 03:41:27 -0300

    Include zsh autosuggestions

--- init.zsh
+++ init.zsh
@@ -6,3 +6,5 @@
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
 source $(dirname $0)/titles.plugin.zsh
+source $(dirname $0)/zsh-autosuggestions/zsh-autosuggestions.zsh
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=30
